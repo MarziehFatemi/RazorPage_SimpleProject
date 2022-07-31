89704fa739fd96b058249750453dc61dbb72d767
marziehfatemi@gmail.com <marziehfatemi@gmail.com>
2022-07-31 13:27:25 +0430

    ProjectViewModelCreated

---
+++ testrazor2/Models/ProjectViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace RazorBlog.Models
+{
+    public class ProjectViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDiscription { get; set; }
+        public string Client { get; set; }
+        public string Image { get; set; }
+        public string Body { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle { get; set; }
+        public string CreationDate { get; set; }
+
+
+    }
+}

68532170e7d5bcee82e70cc5cdae4f2c5a54dadb
marziehfatemi@gmail.com <marziehfatemi@gmail.com>
2022-07-31 11:50:50 +0430

    Project Model Created

---
+++ testrazor2/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿namespace RazorBlog_.NetCore.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDisciption { get; set; }
+        public string Client { get; set; }
+        public string Image { get; set; }
+        public string Body { get; set; }
+        public string PictureAlt { get; set; }
+        public string  PictureTitle { get; set; }
+        public DateTime CreationDate { get; set; }
+
+
+        public Project(long id, string name, DateTime creationDate , string client, 
+            string image, string pictureAlt, string pictureTitle,
+            string shortDiscription, string body)
+        {
+            Id = id;
+            Name = name;
+            ShortDisciption = shortDiscription;
+            Client = client;
+            Image = image;
+            Body = body;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            CreationDate = creationDate; 
+        }
+    }
+}
